011dcbffca78f0bce99a8f8c6867d10ca108db82
Austen Allred <austen.allred@gmail.com>
2015-01-21 17:43:22 -0700

    intial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var totalBillField: UITextField!
+    @IBOutlet var tipAmountSegmentedControl: UISegmentedControl!
+    @IBOutlet var tipTotalLabel: UILabel!
+    @IBOutlet var calculateTipButton: UIButton!
+    @IBAction func calculateTip(sender: AnyObject) {
+        var userInput = totalBillField.text as NSString
+        var userInputFloat = userInput.floatValue
+        var index: Int = tipAmountSegmentedControl.selectedSegmentIndex
+        var tipRate: Float = 0.15
+        if index == 0 {
+            tipRate = 0.15
+        }
+        else if index == 1 {
+            tipRate = 0.20
+        }
+        else {
+            tipRate = 0.25
+        }
+        var tip: Float = userInputFloat * tipRate
+        tipTotalLabel.text = "$\(tip)"
+        
+       println("user input is \(userInput)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
